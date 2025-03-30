176a33b63c5ce75963e5ac6fdd5d3bbbfdb2b32b
nangirakimberly09 <nangirakimberly09@gmail.com>
2025-03-30 08:15:49 +0300

    Modify server.js to prevent listen during tests

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>MILESTONE 2</h1><p>Hello, World!</p>');
+  res.send('<h1>MILESTONE 3</h1><h1>MILESTONE 2</h1><p>Hello, World!</p>');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+module.exports = app;
