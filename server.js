66aed37ce7fd93a50aa4af441e40fcb4e6f955be
nangirakimberly09 <nangirakimberly09@gmail.com>
2025-03-31 19:47:44 +0300

    Add MILESTONE 4 to landing page

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>MILESTONE 3</h1><h1>MILESTONE 2</h1><p>Hello, World!</p>');
+  res.send('<h1>MILESTONE 4</h1><h1>MILESTONE 3</h1><h1>MILESTONE 2</h1><p>Hello, World!</p>');
 });
 
 if (require.main === module) {
